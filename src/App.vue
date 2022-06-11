607cec5d09d842c6855cb041be84c386174b6649
Rodos <99319617+BogacSevin@users.noreply.github.com>
2022-06-11 22:23:01 +0300

    -1

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <login />
+    <changePassword/>
+  </div>
+</template>
+
+<script>
+import login from './components/login.vue'
+import changePassword from './components/changePassword.vue'
+
+export default {
+  name: 'App',
+  components: {
+    login,
+    changePassword
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+</style>
---
+++ src/components/login.vue
@@ -0,0 +1,85 @@
+<template>
+  <div style="
+  margin-left: auto;
+  margin-right: auto;
+  width: 50em;
+  height: 50rem;
+  margin-top: 15%;
+  text-align: left;">
+    <b-form @submit.prevent="onCreatePost" @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          placeholder="Enter email"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group id="input-group-2" label="Your Password:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.password"
+          placeholder="Enter password"
+          required
+          type="password"
+        ></b-form-input>
+      </b-form-group>
+          <label class="mt-2" @click="$bvModal.show('bv-modal-example')"> Şifremi Unuttum? </label>
+  <b-modal id="bv-modal-example" hide-footer>
+    <div>
+  <b-form inline>
+    <label class="sr-only" for="inline-form-input-username">Email</label>
+    <b-input-group  class="mb-2 mr-sm-2 mb-sm-0">
+      <b-form-input id="inline-form-input-username" placeholder="Email"></b-form-input>
+    </b-input-group>
+    <b-button variant="primary" class="mt-4" block @click="$bvModal.hide('bv-modal-example')" >Şifremi Yenile</b-button>
+  </b-form>
+</div>
+  </b-modal>       
+        <div style="margin-top:2rem; ">
+      <b-button type="submit" style="padding-rigt:0.5rem;" variant="primary">Submit</b-button>
+        </div>
+    </b-form>
+  </div>
+</template>
+    <!-- <b-button class="mt-3" block @click="$bvModal.hide('bv-modal-example')">Close Me</b-button> -->
+<script>
+import axios from 'axios';
+  export default {
+    data() {
+      return {
+        form: {
+          email: '',
+          password: '',
+        },
+        show: true,
+      }
+    },
+    methods: {
+      onCreatePost(){
+        axios.post('https://dev-smoothie-api.fintechyazilim.com/api/User/Login',
+        {email: this.email, password: this.password}
+        ).then(response => {
+          console.log(response);
+        });
+      },
+      onSubmit(event) {
+        event.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+      onReset(event) {
+        event.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.password = ''
+      }
+    }
+  }
+</script>
+
